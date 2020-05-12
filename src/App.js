a871c4f9dd8965f8c28c58abaf17d9ae84068b27
Dominicushuy <huypro1996z@gmail.com>
2020-05-12 19:39:18 +0700

    complete project

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Home from "./pages/HomePage";
+import About from "./pages/AboutPage";
+import Products from "./pages/ProductsPage";
+import Contact from "./pages/ContactPage";
+import SingleProduct from "./pages/SingleProductPage";
+import Cart from "./pages/CartPage.js";
+import Default from "./pages/Default.js";
+
+import { Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import SideCart from "./components/SideCart";
+import Footer from "./components/Footer";
+class App extends Component {
+  render() {
+    return (
+      <>
+        {/* navbar, sidebar, cart,footer*/}
+        <Navbar />
+        <Sidebar />
+        <SideCart />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/products/:id" component={SingleProduct} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+        <Footer />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/CartPage/CartTotals.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { ProductConsumer } from "../../context";
+// import PayPalBtn from "./PayPalBtn";
+export default function CartTotals({ history }) {
+  return (
+    <div className="container">
+      <div className="row">
+        <ProductConsumer>
+          {value => {
+            const { clearCart, cartSubTotal, cartTax, cartTotal } = value;
+
+            return (
+              <div className="col text-title text-center my-4">
+                <button
+                  className="btn btn-outline-danger text-capitalize mb-4"
+                  onClick={clearCart}
+                >
+                  clear cart
+                </button>
+                <h3>subtotal : ${cartSubTotal}</h3>
+                <h3>tax : ${cartTax}</h3>
+                <h3>total : ${cartTotal}</h3>
+                {/* <PayPalBtn
+                  history={history}
+                  cartTotal={cartTotal}
+                  clearCart={clearCart}
+                /> */}
+              </div>
+            );
+          }}
+        </ProductConsumer>
+      </div>
+    </div>
+  );
+}
